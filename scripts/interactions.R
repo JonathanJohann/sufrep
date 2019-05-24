861899b0f33a8c6648ffb0d982ede6444547d6ee
Jonathan Johannemann <JonathanJohannemann@gmail.com>
2019-05-24 15:43:40 -0700

    using same evaluation function for real datasets for the simulation datasets

--- scripts/interactions.R
+++ scripts/interactions.R
@@ -21,64 +21,40 @@ config <- expand.grid(
   iterations=1,
   signal_fraction=c(0.5),
   comp_percent=c(0.9),
-  num_levels=c(500,100),
+  num_levels=c(100,500),
   num_interactions=c(1)
 )
 
-shuffled.indices <- sample(dim(config)[1]-1,
-                           size=dim(config)[1]-1,
-                           replace=FALSE)
-
-config <- config[c(1,(shuffled.indices+1)),]
+model = "regression_forest"
 
 encodings <- c("MNL","one_hot","multi_permutation","permutation",
                "difference","helmert","fisher","deviation",
                "low_rank","sparse_low_rank","repeated_effect",
                "means","simple_effect")
 
-walk(seq(dim(config)[1]), function(i) {
+seed <- time_seed()
 
+for(i in 1:dim(config)[1]){
+  
   cfg <- config[i,]
-
-  output <- rerun(.n=cfg$iterations, {
-
-    seed <- time_seed()
-    sim_data <- make_dataset(setup=as.character(cfg$setup),
-                             n=cfg$n,p=cfg$p,
-                             rho=cfg$rhos,
-                             size_l=cfg$num_groups,
-                             size_a=cfg$num_levels,
-                             lambda=cfg$signal_fraction,
-                             snr=cfg$noise_ratio,
-                             comp_percent=cfg$comp_percent,
-                             seed=seed,
-                             num_interactions = cfg$num_interactions,
-                             test=TRUE)
-
-    mses <- matrix(0,nrow=1,ncol=13)
-    print("Starting...")
-    train = data.frame(sim_data$TRAIN %>% dplyr::mutate_at("A",fix_factors))
-    test = data.frame(sim_data$TEST %>% dplyr::mutate_at("A",fix_factors))
-
-    for(ii in 1:13){
-      if(encodings[ii]=="fisher"){
-        mses[ii] <- evaluate(method=encodings[ii],train=train,test=test,categorical = "A",response="Y",Y="Y")
-      } else if(encodings[ii] %in% c("low_rank","sparse_low_rank")) {
-        mses[ii] <- evaluate(method=encodings[ii],train=train,test=test,categorical = "A",response="Y",num_components=3)
-      } else {
-        mses[ii] <- evaluate(method=encodings[ii],train=train,test=test,categorical = "A",response="Y")
-      }
-      print(ii)
-    }
-
-    colnames(mses) <- encodings
-    result <- cbind(cfg,mses)
-    result[,"seed"] <- seed
-    as_tibble(result)
-  }) %>% bind_rows()
-
-  # Write to csv
-  filename <- paste(cfg$setup,"_n", cfg$n,
+  sim_data <- make_dataset(setup=as.character(cfg$setup),
+                           n=cfg$n,p=cfg$p,
+                           rho=cfg$rhos,
+                           size_l=cfg$num_groups,
+                           size_a=cfg$num_levels,
+                           lambda=cfg$signal_fraction,
+                           snr=cfg$noise_ratio,
+                           comp_percent=cfg$comp_percent,
+                           seed=seed,
+                           num_interactions = cfg$num_interactions,
+                           test=TRUE)
+  
+  train = data.frame(sim_data$TRAIN %>% dplyr::mutate_at("A",fix_factors))
+  test = data.frame(sim_data$TEST %>% dplyr::mutate_at("A",fix_factors))
+  
+  result = evaluate_method(rbind(train,test),categorical="A",response="Y",k=4,model=model)
+  result[,"seed"] <- seed
+  filename <- paste(model,"_",cfg$setup,"_n", cfg$n,
                     "_p",cfg$p,
                     "_rhos",cfg$rhos,
                     "_snr", cfg$noise_ratio,
@@ -88,6 +64,6 @@ walk(seq(dim(config)[1]), function(i) {
                     "_", time_seed(),
                     ".csv",
                     sep="")
-  write_csv(output, filename)
-})
+  write.csv(result,file = filename)
+}
 
--- scripts/linear.R
+++ scripts/linear.R
@@ -12,7 +12,7 @@ source("utils.R")
 
 config <- expand.grid(
   setup="linear",
-  num_groups=c(10),
+  num_groups=c(2,10),
   n=c(10000),
   p=c(20),
   rhos=c(0.25),
@@ -21,61 +21,40 @@ config <- expand.grid(
   iterations=1,
   signal_fraction=c(0.5),
   comp_percent=c(0.9),
-  num_levels=c(500),
+  num_levels=c(100,500),
   num_interactions=c(1)
 )
 
-
-config <- config[sample(c(1:dim(config)[1]),size=1,replace=FALSE),]
+model = "regression_forest"
 
 encodings <- c("MNL","one_hot","multi_permutation","permutation",
                "difference","helmert","fisher","deviation",
                "low_rank","sparse_low_rank","repeated_effect",
                "means","simple_effect")
 
-walk(seq(dim(config)[1]), function(i) {
+seed <- time_seed()
 
+for(i in 1:dim(config)[1]){
+  
   cfg <- config[i,]
-
-  output <- rerun(.n=cfg$iterations, {
-
-    seed <- time_seed()
-    sim_data <- make_dataset(setup=as.character(cfg$setup),
-                             n=cfg$n,p=cfg$p,
-                             rho=cfg$rhos,
-                             size_l=cfg$num_groups,
-                             size_a=cfg$num_levels,
-                             lambda=cfg$signal_fraction,
-                             snr=cfg$noise_ratio,
-                             comp_percent=cfg$comp_percent,
-                             seed=seed,
-                             num_interactions = cfg$num_interactions,
-                             test=TRUE)
-
-    mses <- matrix(0,nrow=1,ncol=13)
-    print("Starting...")
-    train = data.frame(sim_data$TRAIN %>% dplyr::mutate_at("A",fix_factors))
-    test = data.frame(sim_data$TEST %>% dplyr::mutate_at("A",fix_factors))
-
-    for(ii in 1:13){
-      if(encodings[ii]=="fisher"){
-        mses[ii] <- evaluate(method=encodings[ii],train=train,test=test,categorical = "A",response="Y",Y="Y")
-      } else if(encodings[ii] %in% c("low_rank","sparse_low_rank")) {
-        mses[ii] <- evaluate(method=encodings[ii],train=train,test=test,categorical = "A",response="Y",num_components=3)
-      } else {
-        mses[ii] <- evaluate(method=encodings[ii],train=train,test=test,categorical = "A",response="Y")
-      }
-      print(ii)
-    }
-
-    colnames(mses) <- encodings
-    result <- cbind(cfg,mses)
-    result[,"seed"] <- seed
-    as_tibble(result)
-  }) %>% bind_rows()
-
-  # Write to csv
-  filename <- paste(cfg$setup,"_n", cfg$n,
+  sim_data <- make_dataset(setup=as.character(cfg$setup),
+                           n=cfg$n,p=cfg$p,
+                           rho=cfg$rhos,
+                           size_l=cfg$num_groups,
+                           size_a=cfg$num_levels,
+                           lambda=cfg$signal_fraction,
+                           snr=cfg$noise_ratio,
+                           comp_percent=cfg$comp_percent,
+                           seed=seed,
+                           num_interactions = cfg$num_interactions,
+                           test=TRUE)
+  
+  train = data.frame(sim_data$TRAIN %>% dplyr::mutate_at("A",fix_factors))
+  test = data.frame(sim_data$TEST %>% dplyr::mutate_at("A",fix_factors))
+  
+  result = evaluate_method(rbind(train,test),categorical="A",response="Y",k=4,model=model)
+  result[,"seed"] <- seed
+  filename <- paste(model,"_",cfg$setup,"_n", cfg$n,
                     "_p",cfg$p,
                     "_rhos",cfg$rhos,
                     "_snr", cfg$noise_ratio,
@@ -85,6 +64,6 @@ walk(seq(dim(config)[1]), function(i) {
                     "_", time_seed(),
                     ".csv",
                     sep="")
-  write_csv(output, filename)
-})
+  write.csv(result,file = filename)
+}
 
