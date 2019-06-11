eecec12beae9ec239125836ef47290a95d8c4fdf
Jonathan Johannemann <JonathanJohannemann@gmail.com>
2019-06-11 00:52:58 -0700

    Add files via upload

--- scripts/evaluation.R
+++ scripts/evaluation.R
@@ -1,38 +1,91 @@
 library(sufrep)
 library(grf)
-
+library(caret)
 time_seed <- function() {
   as.integer((as.numeric(Sys.time()) * 1e+07) %% 1e+07)
 }
-encoding_methods <- c("means")
+encoding_methods <- c("multi_permutation")
 method <- sample(encoding_methods, 1)
-n <- 1000
-p <- 10
-k <- 5 #sample(c(2, 5))
-ngl <- 100 #sample(c(5, 10, 25, 50, 100), 1)
+n <- 10000
+p <- 20
+k <- 10
+ngl <- 100#sample(c(50, 100), 1)
 pl <- .9
-type <- "hermite"
+type <- "global"
 filename <- paste0(method, "_", time_seed(), ".csv", collapse = "")
 
-for (i in seq(100)) {
+num_comp <- c(5,10,15)
+
+start <- Sys.time()
+for (i in seq(10)) {
   print(i)
-
-  data <- create_data(n, p, k, ngl = ngl, pl = pl, type = type)
-
-  enc_method <- make_encoder(method, X = data$x, G = data$g)
-  enc_onehot <- make_encoder("one_hot", X = data$x, G = data$g)
-
-  x_enc <- enc_method(data$x, data$g)
-  x_onehot <- enc_onehot(data$x, data$g)
-
-  forest_enc <- regression_forest(x_enc, data$y)
-  forest_onehot <- regression_forest(x_onehot, data$y)
-
-  mse_enc <- mean(predict(forest_enc)$debiased.error, na.rm = TRUE)
-  mse_onehot <- mean(predict(forest_onehot)$debiased.error, na.rm = TRUE)
-
-  config <- cbind(n, p, k, ngl, pl, method, other = NA, mse_enc, mse_onehot)
-  write.table(config, file = filename, append = T, col.names = F, sep = ",")
-  print(config)
-
+  try({
+    data <- create_data(n, p, k, ngl = ngl, pl = pl, type = type)
+    train_test <- createDataPartition(factor(data$g),p=0.8)
+    
+    train <- list(x = data$x[train_test$Resample1,],
+                  g = data$g[train_test$Resample1],
+                  y = data$y[train_test$Resample1])
+    
+    test <- list(x = data$x[-train_test$Resample1,],
+                 g = data$g[-train_test$Resample1],
+                 y = data$y[-train_test$Resample1])
+    
+    if(method %in% c("fisher")){
+      enc_method <- make_encoder(method, X = train$x, G = train$g,Y=train$y)
+      x_enc <- enc_method(train$x, train$g)
+      x_test_enc <- enc_method(test$x, test$g)
+    }
+    else if(method %in% c("multi_permutation")){
+      enc_method <- make_encoder(method, X = train$x, G = train$g,num_permutations = p)
+      x_enc <- enc_method(train$x, train$g)
+      x_test_enc <- enc_method(test$x, test$g)
+    }
+    else if(method %in% c("low_rank","sparse_low_rank")){
+      folds <- createFolds(factor(train$g),k = 3,returnTrain = T)
+      cv_mses <- c()
+      for(ii in 1:length(num_comp)){
+        cmp <- num_comp[ii]
+        fold_mses <- c()
+        for(iii in 1:3){
+          #make encoder function based on training subset
+          enc_method_CV <- make_encoder(method, X = train$x[folds[[iii]],], G = train$g[folds[[iii]]],num_components = cmp)
+          
+          #CV train & test
+          x_enc_CV <- enc_method_CV(train$x[folds[[iii]],], train$g[folds[[iii]]])
+          
+          x_test_enc_CV <- enc_method_CV(train$x[-folds[[iii]],], train$g[-folds[[iii]]])
+          
+          forest_enc_CV <- regression_forest(x_enc_CV, train$y[folds[[iii]]])
+          mse_enc_CV <- mean((predict(forest_enc_CV,x_test_enc_CV)$predictions - train$y[-folds[[iii]]])^2, na.rm = TRUE)
+          fold_mses <- c(fold_mses,mse_enc_CV)
+          print(paste0("CV ... ",iii))
+        }
+        cv_mses <- c(cv_mses,mean(fold_mses))
+        print(paste0("Checking - ",ii))
+      }
+      enc_method <- make_encoder(method, X = train$x, G = train$g,num_components = which.min(cv_mses))
+      x_enc <- enc_method(train$x, train$g)
+      x_test_enc <- enc_method(test$x, test$g)
+    }
+    else{
+      enc_method <- make_encoder(method, X = train$x, G = train$g)
+      x_enc <- enc_method(train$x, train$g)
+      x_test_enc <- enc_method(test$x, test$g)
+    }
+    enc_onehot <- make_encoder("one_hot", X = train$x, G = train$g)
+    x_onehot <- enc_onehot(train$x, train$g)
+    x_test_onehot <- enc_onehot(test$x,test$g)
+    
+    forest_enc <- regression_forest(x_enc, train$y)
+    forest_onehot <- regression_forest(x_onehot, train$y)
+    
+    mse_enc <- mean((predict(forest_enc,x_test_enc)$predictions - test$y)^2, na.rm = TRUE)
+    mse_onehot <- mean((predict(forest_onehot,x_test_onehot)$predictions - test$y)^2, na.rm = TRUE)
+    
+    config <- cbind(n, p, k, ngl, pl, method, other = NA, mse_enc, mse_onehot)
+    write.table(config, file = filename, append = T, col.names = F, sep = ",")
+  })
+  end <- Sys.time()
+  print(end-start)
 }
